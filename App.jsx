d91cca4965b3dfb09d824d789dcc19a83dd44078
Nao Takegawa <nao.takegawa316@gmail.com>
2017-10-06 16:41:05 +0900

    finished https://www.tutorialspoint.com/reactjs/reactjs_component_api.htm

--- App.jsx
+++ App.jsx
@@ -1,10 +1,29 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class App extends React.Component {
+   constructor() {
+      super();
+
+      this.state = {
+         data: 1
+      }
+
+      this.setStateHandler = this.setStateHandler.bind(this);
+   };
+
+   setStateHandler() {
+      var item = 4
+      var num = this.state.data;
+      var changedNum = num + 1;
+      this.setState({data: changedNum})
+   };
+
    render() {
       return (
          <div>
-            Hello World!!!
+            <button onClick = {this.setStateHandler}>SET STATE</button>
+            <h4>State Array: {this.state.data}</h4>
          </div>
       );
    }
