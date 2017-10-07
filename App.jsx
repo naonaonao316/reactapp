78649604808579d13c98873ecc61d992f61deb40
Nao Takegawa <nao.takegawa316@gmail.com>
2017-10-07 15:44:13 +0900

    finished 'Simple Example (https://www.tutorialspoint.com/reactjs/reactjs_events.htm)'

--- App.jsx
+++ App.jsx
@@ -3,38 +3,25 @@ import PropTypes from 'prop-types';
 
 class App extends React.Component {
   constructor(props) {
-    super(props);
+     super(props);
 
-    this.state = {
-      data: 'Initial data'
-    }
-
-    this.updateState = this.updateState.bind(this);
+     this.state = {
+        data: 'Initial data...'
+     }
+     this.updateState = this.updateState.bind(this);
   };
 
-  updateState(e) {
-    this.setState({data: e.target.value});
+  updateState() {
+     this.setState({data: 'Data updated...'})
   }
 
   render() {
-    return (
-      <div>
-        <Content myDataProp = {this.state.data}
-          updateStateProp = {this.updateState}></Content>
-      </div>
-    );
-  }
-}
-
-class Content extends React.Component {
-  render() {
-    return (
-      <div>
-        <input type = "text" value = {this.props.myDataProp}
-          onChange = {this.props.updateStateProp} />
-        <h3>{this.props.myDataProp}</h3>
-      </div>
-    );
+     return (
+        <div>
+           <button onClick = {this.updateState}>CLICK</button>
+           <h4>{this.state.data}</h4>
+        </div>
+     );
   }
 }
 
