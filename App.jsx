d19b85d7c07e828ba569747b8692841ad729a418
Nao Takegawa <nao.takegawa316@gmail.com>
2017-10-07 17:25:25 +0900

    in the middle of 'Router (https://www.tutorialspoint.com/reactjs/reactjs_router.htm)'

--- App.jsx
+++ App.jsx
@@ -1,50 +1,49 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
+import {Router, Route, browserHistory, IndexRoute} from 'react-router';
 
-class App extends React.Component {
-  constructor() {
-     super();
-
-     this.state = {
-        data: [
-          {
-            component: 'First...',
-            id: 1
-          },
-          {
-            component: 'Second...',
-            id: 2
-          },
-          {
-            component: 'Third...',
-            id: 3
-          }
-        ]
-     }
-  };
-
+export default class App extends React.Component {
   render() {
-     return (
-        <div>
-          <div>
-            {this.state.data.map((dynamicComponent, i) => <Content
-                key = {i} componentData = {dynamicComponent}/>)}
-          </div>
-        </div>
-     );
+    return (
+      <div>
+        <ul>
+          <li><Link to = "/home">Home</Link></li>
+          <li><Link to = "/about">About</Link></li>
+          <li><Link to = "/contact">Contact</Link></li>
+        </ul>
+        {this.props.children}
+      </div>
+    )
   }
 }
 
-class Content extends React.Component {
+export class Home extends React.Component {
    render() {
       return (
          <div>
-           <div>{this.props.componentData.component}</div>
-           <div>{this.props.componentData.id}</div>
+            <h1>Home...</h1>
          </div>
-      );
+      )
    }
 }
 
-export default App;
+export class About extends React.Component {
+   render() {
+      return (
+         <div>
+            <h1>About...</h1>
+         </div>
+      )
+   }
+}
+
+export class Contact extends React.Component {
+   render() {
+      return (
+         <div>
+            <h1>Contact...</h1>
+         </div>
+      )
+   }
+}
--- main.js
+++ main.js
@@ -1,5 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Router, Route, browserHistory, IndexRoute} from 'react-router';
 import App from './App.jsx';
+import {Home, About, Contact} from './App.jsx';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+
+ReactDOM.render((
+   <Router history = {browserHistory}>
+      <Route path = "/" component = {App}>
+         <IndexRoute component = {Home} />
+         <Route path = "home" component = {Home} />
+         <Route path = "about" component = {About} />
+         <Route path = "contact" component = {Contact} />
+      </Route>
+   </Router>
+
+), document.getElementById('app'))
